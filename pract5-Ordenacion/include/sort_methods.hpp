836fcec5a579866322536997d51495981ae01fe5
HimarManuel <xavieryanquin@gmail.com>
2019-04-18 20:58:30 +0100

    Algoritmos implementados

--- pract5-Ordenacion/include/sort_methods.hpp
+++ pract5-Ordenacion/include/sort_methods.hpp
@@ -46,3 +46,105 @@ void sort_by_change (CLAVE* v, int tam) {
       fin = cam -1;
    }
 }
+
+template <class CLAVE>
+void baja (int i, CLAVE* v, int tam) {
+   CLAVE x = v[i];
+   while (2*i < tam) {
+      int h;
+      int h1 = 2*i;
+      int h2 = h1 + 1;
+
+      if (h1 == tam-1 || v[h1] > v[h2]) {
+         h = h1;
+      } else {
+         h = h2;
+      }
+
+      if (v[h] <= x) break;
+      else {
+         v[i] = v[h];
+         v[h] = x;
+         i = h;
+      }
+   }
+}
+
+template <class CLAVE>
+void sort_by_heapsort (CLAVE* v, int tam) {
+   for (int i = tam/2-1; i >= 0; i--) {
+      baja(i, v, tam);
+   }
+
+   for (int i = tam-1; i >= 0; i--) {
+      CLAVE x = v[0];
+      v[0] = v[i];
+      v[i] = x;
+      baja(0,v,i);
+   }
+}
+
+template <class CLAVE>
+void merge (CLAVE* v, int tam, int ini, int cen, int fin) {
+   int i = ini; int j = cen + 1; int k = ini;
+   CLAVE* aux = new CLAVE [tam];
+   while ((i <= cen) && (j <= fin)) {
+      if (v[i] < v[j]) {
+         aux[k] = v[i];
+         i++;
+      } else {
+         aux[k] = v[j];
+         j++;
+      }
+      k++;
+   }
+
+   if (i > cen) {
+      while (j <= fin) {
+         aux[k] = v[j];
+         j++; k++;
+      }
+   } else {
+      while (i <= cen) {
+         aux[k] = v[i];
+         i++; k++;
+      }
+   }
+   for (int s = ini; s <= fin; s++) {
+      v[s] = aux[s];
+   }
+}
+
+template <class CLAVE>
+void sort_by_mergesort (CLAVE* v, int tam, int ini, int fin) {
+   if (ini < fin) {
+      int cen = (ini + fin) / 2;
+      sort_by_mergesort(v, tam, ini, cen);
+      sort_by_mergesort(v, tam, cen+1, fin);
+      merge(v, tam, ini, cen, fin);
+   }
+}
+
+template <class CLAVE>
+void deltaSort(int delta, CLAVE* v, int tam) {
+   int j = 0;
+   for (int i = delta; i < tam; i++) {
+      CLAVE x = v[i];
+      j = i;
+      while ((j >= delta) && (x < v[j-delta])) {
+         v[j] = v[j-delta];
+         j = j - delta;
+      }
+      v[j] = x;
+   }
+}
+
+template <class CLAVE>
+void sort_by_shellSort (CLAVE* v, int tam, int delta) {
+   std::cout << "delta: " << delta << '\n';
+   int del = delta;
+   while (del > 0) {
+      del = del / 2;
+      deltaSort(del, v, tam);
+   }
+}
--- pract5-Ordenacion/src/main.cpp
+++ pract5-Ordenacion/src/main.cpp
@@ -6,6 +6,20 @@
 #include "../include/sort_methods.hpp"
 #include "../include/DNI.hpp"
 
+bool is_equal (DNI* v1, DNI* v2, int tam) {
+   for (int i = 0; i < tam; i++) {
+      if (v1[i] != v2[i]) return false;
+   }
+   return true;
+}
+
+bool is_correct (DNI* v, int tam) {
+   for (int i = 1; i < tam; i++) {
+      if (v[i-1] > v[i]) return false;
+   }
+   return true;
+}
+
 int main (void) {
    srand(time(NULL));
    int n;
@@ -18,7 +32,7 @@ int main (void) {
       std::cout << v_dni[i] << " ";
       v_dni2[i] = v_dni[i];
    }
-   std::cout << "\nEjecutando el algoritmo de seleccion...\n";
+  /* std::cout << "\nEjecutando el algoritmo de seleccion...\n";
    sort_by_selection(v_dni,n);
    std::cout << "Vector ordenado: ";
    for (int i = 0; i < n; i++) {
@@ -35,6 +49,32 @@ int main (void) {
    for (int i = 0; i < n; i++) {
       std::cout << v_dni2[i] << " ";
    }
-   std::cout << '\n';
-   std::cout << "Los resultados " << (v_dni == v_dni2 ? "" : "no ") << "coinciden\n";
+   std::cout << '\n';*/
+   /*std::cout << "\nEjecutando ordenacion heapsort...\n";
+   sort_by_heapsort(v_dni,n);
+   std::cout << "Vector ordenado: ";
+   for (int i = 0; i < n; i++) {
+      std::cout << v_dni[i] << " ";
+   }
+   if (is_correct(v_dni,n)) std::cout << "CORRECTO\n"; else std::cout << "INCORRECTO\n";*/
+
+  /* std::cout << "\nEjecutando ordenacion mergesort...\n";
+   sort_by_mergesort(v_dni,n, 0, n-1);
+   std::cout << "Vector ordenado: ";
+   for (int i = 0; i < n; i++) {
+      std::cout << v_dni[i] << " ";
+   }
+   if (is_correct(v_dni,n)) std::cout << "CORRECTO\n"; else std::cout << "INCORRECTO\n";*/
+
+    std::cout << "\nEjecutando ordenacion shellSort...\n";
+    std::cout << "Indica el valor de alpha (0 < alpha < 1): ";
+    float alpha;
+    std::cin >> alpha;
+    sort_by_shellSort(v_dni,n, alpha*n);
+    std::cout << "Vector ordenado: ";
+    for (int i = 0; i < n; i++) {
+       std::cout << v_dni[i] << " ";
+    }
+    if (is_correct(v_dni,n)) std::cout << "CORRECTO\n"; else std::cout << "INCORRECTO\n";
+   return 0;
 }
